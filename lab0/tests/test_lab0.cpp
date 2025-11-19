86f6d74ded87db155e001ac4517469e941097a68
Victoria <153975106+victoriammmm@users.noreply.github.com>
2025-11-19 23:17:23 +0700

    меняю include

--- lab0/tests/test_lab0.cpp
+++ lab0/tests/test_lab0.cpp
@@ -1,4 +1,6 @@
-#include "lab0.h"
+#include "filereader.h"
+#include "writetocsv.h"
+#include "wordcounter.h"
 #include <gtest/gtest.h>
 #include <fstream>
 
@@ -143,3 +145,4 @@ int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
