327ddf52ba3fa9e7ee1ce6adf6442b8ba57e576a
Victoria <153975106+victoriammmm@users.noreply.github.com>
2025-12-23 20:31:14 +0700

    Update and rename test_lab0.cpp to TestLab0.cpp

--- lab0/tests/TestLab0.cpp
+++ lab0/tests/TestLab0.cpp
@@ -1,6 +1,6 @@
-#include "filereader.h"
-#include "writetocsv.h"
-#include "wordcounter.h"
+#include "FileReader.h"
+#include "WriteToCsv.h"
+#include "WordCounter.h"
 #include <gtest/gtest.h>
 #include <fstream>
 
@@ -145,4 +145,5 @@ int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
 
